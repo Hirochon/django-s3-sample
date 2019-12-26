865b0f87834490c178b388b8767a6afb3efc19b7
Hirochon <heacet43@gmail.com>
2019-12-27 01:19:05 +0900

    [add] logging setting

--- config/settings/local.py
+++ config/settings/local.py
@@ -101,3 +101,50 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Logging!!
+
+if DEBUG:
+    LOGGING = {
+        'version' : 1,                          #バージョンは1に固定
+        'disable_existing_loggers' : False,     #既存のログ設定を無効化しない
+
+        #ログフォーマット
+        'formatters' : {
+            #開発用
+            'develop' : {
+                'format' : '%(asctime)s [%(levelname)s] %(pathname)s:%(lineno)d %(message)s'
+            },
+        },
+        #ハンドラ
+        'handlers' : {
+            #コンソール出力用ハンドラ
+            'console' : {
+                'level' : 'DEBUG',
+                'class' : 'logging.StreamHandler',
+                'formatter' : 'develop',
+            },
+        },
+        #ロガー
+        'loggers' : {
+            #自作アプリケーション全般のログを拾うロガー
+            '' : {
+                'handlers' : ['console'],
+                'level' : 'DEBUG',
+                'propagate' : False,
+            },
+            #Django本体が出すログ全般を拾うロガー
+            'django' : {
+                'handlers' : ['console'],
+                'level' : 'INFO',
+                'propagate' : False,
+            },
+            #発行されるSQL文を出力するための設定
+            'django.db.backends' : {
+                'handlers' : ['console'],
+                'level' : 'DEBUG',
+                'propagate' : False,
+            },
+        },
+    }
